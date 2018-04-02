6566be37feaf36dcad11066ceccf9b75ea0a7d54
higoroliveira <higor.oliveira@afferolab.com.br>
2018-04-02 18:01:14 -0300

    Fechando Tutorial

--- src/components/github/ReposContainer.js
+++ src/components/github/ReposContainer.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react'
 import { fetchRepos } from '../../services/repos-api'
+import ReposList from './ReposList'
 
 // stateful component
 class ReposContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            repos: []
+            repos: [],
+            username: ''
         }
+        this.changeHandler = this.changeHandler.bind(this)
+        this.submitHandler = this.submitHandler.bind(this)
     }
 
-    componentDidMount() {
-        fetchRepos('vedovelli').then(res => window.console.log(res.data))
+    changeHandler(ev) {        
+        this.setState({ username: ev.target.value })
+    }
+
+    submitHandler(ev) { 
+        ev.preventDefault()       
+        fetchRepos(this.state.username).then(res => this.setState({ repos: res.data }))
     }
 
     render () {
         return (
-            <h1>Repos Container</h1>
+           <div>
+            <h1>Repos</h1>
+            <form action="#" onSubmit={this.submitHandler}>
+                <input 
+                onChange={this.changeHandler}
+                style={{width: '250px'}} 
+                type="search" placeholder="Informe usuário e tecle ENTER..."/>
+            </form>
+            <ReposList repos={this.state.repos}></ReposList>
+           </div>
         )
     }
 }
